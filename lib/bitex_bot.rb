dc4f95f1af5b51433d2d27a85a76f3824fbdd277
Juan Szymczak <juanangelszymczak@gmail.com>
2018-06-19 17:14:31 -0300

    set new structure

--- lib/bitex_bot.rb
+++ lib/bitex_bot.rb
@@ -17,8 +17,9 @@ require 'itbit'
 # BitexBot Models
 require 'bitex_bot/settings'
 require 'bitex_bot/database'
-require 'bitex_bot/models/apis/api_wrapper.rb'
-Dir[File.dirname(__FILE__) + '/bitex_bot/models/apis/**/*.rb'].each { |file| require file }
+require 'bitex_bot/api.rb'
+require 'bitex_bot/api/api_wrapper.rb'
+Dir[File.dirname(__FILE__) + '/bitex_bot/api/**/*.rb'].each { |file| require file }
 require 'bitex_bot/models/opening_flow.rb'
 require 'bitex_bot/models/closing_flow.rb'
 Dir[File.dirname(__FILE__) + '/bitex_bot/models/*.rb'].each { |file| require file }
---
+++ lib/bitex_bot/api.rb
@@ -0,0 +1,63 @@
+module BitexBot
+  module Api
+    MIN_AMOUNT = 5
+
+    Transaction = Struct.new(
+      :id,        # Integer
+      :price,     # Decimal
+      :amount,    # Decimal
+      :timestamp, # Epoch Integer
+      :raw        # Actual transaction
+    )
+
+    Order = Struct.new(
+      :id,        # String
+      :type,      # Symbol
+      :price,     # Decimal
+      :amount,    # Decimal
+      :timestamp, # Integer
+      :raw        # Actual order object
+    ) do
+      def method_missing(method_name, *args, &block)
+        raw.respond_to?(method_name) ? raw.send(method_name, *args, &block) : super
+      end
+
+      def respond_to_missing?(method_name, include_private = false)
+        raw.respond_to?(method_name) || super
+      end
+    end
+
+    OrderBook = Struct.new(
+      :timestamp, # Integer
+      :bids,      # [OrderSummary]
+      :asks       # [OrderSummary]
+    )
+
+    OrderSummary = Struct.new(
+      :price,   # Decimal
+      :quantity # Decimal
+    )
+
+    BalanceSummary = Struct.new(
+      :btc, # Balance
+      :usd, # Balance
+      :fee  # Decimal
+    )
+
+    Balance = Struct.new(
+      :total,    # Decimal
+      :reserved, # Decimal
+      :available # Decimal
+    )
+
+    UserTransaction = Struct.new(
+      :order_id, # Integer
+      :usd,      # Decimal
+      :btc,      # Decimal
+      :btc_usd,  # Decimal
+      :fee,      # Decimal
+      :type,     # Integer
+      :timestamp # Epoch Integer
+    )
+  end
+end
